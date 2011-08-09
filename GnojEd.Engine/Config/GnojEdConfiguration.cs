c211c8cace32a57184f7875d144e39385b1b6765
Jelle de Jong <jelle@infoprojects.nl>
2011-08-09 21:43:09 +0200

    Added a GnojEdConfiguration class (not used yet)

---
+++ GnojEd.Engine/Config/GnojEdConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace GnojEd.Engine.Config {
+  using System;
+  using System.Collections.Generic;
+  using System.Configuration;
+  using System.Linq;
+  using System.Text;
+
+  /// <summary>
+  /// GnojEdConfiguration class
+  /// </summary>
+  public class GnojEdConfiguration : ConfigurationSection {
+  }
+}
